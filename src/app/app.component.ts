b7397781fa32273c266d3ab3f46724af6f3cca85
Esteban <ext.evilches@servipag.cl>
2023-01-17 10:24:50 -0300

    FEAT(): se agregan los botones con los tipos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-host';
+
+  public pokemonName!: string;
+
+  constructor(
+    private fb:FormBuilder,
+  ){ }
+
+  ngOnInit(): void {
+    this.initForm()
+  }
+
+  initForm(){
+
+  }
+
+  
 }
